0450e02f97bddc63e903b767a3c9c30f2721ebff
sabrina-fontana <sabfon@hotmail.it>
2021-03-02 14:57:32 +0100

    creazione db e php foreach

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-## PHP -AJAX - DISCHI
+## PHP - AJAX - DISCHI
 
-**1^ MILESTONE**: *index.php*
+**1^ MILESTONE**: *index.php*\
 Solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
 
-**2^ MILESTONE**: *index.html*
+**2^ MILESTONE**: *index.html*\
 Attraverso l’utilizzo di AJAX: al caricamento della pagina ajax chiederà attraverso una chiamata i dischi a php e li stamperà.
--- index.php
+++ index.php
@@ -2,9 +2,42 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
     <title>dischi - index.php</title>
   </head>
   <body>
+
+    <?php
+      require_once __DIR__ . '/database/database.php';
+    ?>
+
+    <header>
+        <div class="container">
+            <img src="img/logo.png" alt="logo">
+        </div>
+    </header>
+
+    <main>
+      <div class="discs-container container">
+
+        <?php
+        foreach($discs as $disc) {
+        ?>
+
+          <div class="disc">
+              <img src="<?php echo $disc['poster'] ?>" alt="">
+              <h3><?php echo $disc['title'] ?></h3>
+              <span class="author"><?php echo $disc['author'] ?></span>
+              <span class="year"><?php echo $disc['year'] ?></span>
+          </div>
+
+        <?php
+          }
+        ?>
+
+      </div>
+    </main>
 
   </body>
 </html>
