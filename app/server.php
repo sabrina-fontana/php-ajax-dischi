472d2f3e07c2b10aee1beb7c99a5f4e224d921d8
sabrina-fontana <sabfon@hotmail.it>
2021-03-03 16:00:02 +0100

    select per generi. problemi con filtro

--- app/server.php
+++ app/server.php
@@ -1,8 +1,15 @@
 <?php
 require_once __DIR__ . '/../database/database.php';
 
-header('Content-Type: application/json');
-// traduco in json l'array php discs
-$jsonDiscs = json_encode($discs);
+// se nella chiamata http c'è la chiave genre con un valore filtro l'array jsonDiscs
+if (array_key_exists('genre', $_GET) && !empty($_GET['genre'])) {
+  $query = strtolower($_GET['genere']);
+  $dischi = array_filter($dischi, function ($keyValue) use ($query) {
+        return (strpos(strtolower($keyValue['genre']),$query)!== false);
+    });
+}
 
+header('Content-Type: application/json');
+// traduco in json l'array discs
+$jsonDiscs = json_encode($discs);
 echo $jsonDiscs;
--- js/script.js
+++ js/script.js
@@ -1,18 +1,34 @@
 var app = new Vue({
 el: '#app',
 data: {
-  discsArray: []
+  discsArray: [],
+  genresArray: [],
+  genreSelected: ''
 },
 mounted() {
   let that = this;
-
   // chiamata axios a server.php
   axios
   .get("http://localhost/php-ajax-dischi/app/server.php")
   .then(function(resp) {
-    that.discsArray = resp.data;
-    console.log(that.discsArray);
+    // creo array di generi
+    resp.data.forEach((element) => {
+      if (!that.genresArray.includes(element.genre)) {
+        that.genresArray.push(element.genre);
+      }
+    })
+    return that.discsArray = resp.data;
   })
+},
+methods: {
+  changeGenre: function() {
+    let that = this;
+    axios
+    .get("http://localhost/php-ajax-dischi/app/server.php?genre=" + that.genreSelected)
+    .then(function(resp) {
+      return that.discsArray = resp.data;
+    })
+  }
 }
 });
 Vue.config.devtools = true;
